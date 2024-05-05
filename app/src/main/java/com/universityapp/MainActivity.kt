1722d6649f0fff6f9ee1b3f0fa1eaa861848e05b
Prachi Saini <prachi.saini@capgemini.com>
2024-05-05 21:41:01 +0530

    Some Enhancement

--- app/src/main/java/com/universityapp/MainActivity.kt
+++ app/src/main/java/com/universityapp/MainActivity.kt
@@ -20,31 +20,36 @@ class MainActivity :AppCompatActivity() {
     private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = _binding!!.root
-        setContentView(view)
-        //Getting Data from viewModel
-        val viewModel: MainViewModel by viewModels()
-        viewModel.getUniversityViewModel()
-        viewModel.universityList.observe(this, Observer {
-            Toast.makeText(this, "" + it.body(), Toast.LENGTH_LONG).show();
-            binding.recyclerView.layoutManager = LinearLayoutManager(this)
-            binding.recyclerView.adapter =
-                it.body()?.let { it1 ->
-                    UniversityListAdapter(it1, OnClickListener { item ->
-                     val intent=Intent(this,DetailScreen::class.java)
-                        intent.putExtra("name",item.name)
-                        intent.putExtra("state",item.stateProvince)
-                        intent.putExtra("country",item.country)
-                        intent.putExtra("countryCode",item.alphaTwoCode)
-                        intent.putStringArrayListExtra("domains",item.domains)
-                        startActivity(intent)
-                    })
-                }
+        initialiViews()
 
-
-        })
     }
+
+  fun  initialiViews(){
+      _binding = ActivityMainBinding.inflate(layoutInflater)
+      val view = _binding!!.root
+      setContentView(view)
+      //Getting Data from viewModel
+      val viewModel: MainViewModel by viewModels()
+      viewModel.getUniversityViewModel()
+      viewModel.universityList.observe(this, Observer {
+          Toast.makeText(this, "" + it.body(), Toast.LENGTH_LONG).show();
+          binding.recyclerView.layoutManager = LinearLayoutManager(this)
+          binding.recyclerView.adapter =
+              it.body()?.let { it1 ->
+                  UniversityListAdapter(it1, OnClickListener { item ->
+                      val intent=Intent(this,DetailScreen::class.java)
+                      intent.putExtra("name",item.name)
+                      intent.putExtra("state",item.stateProvince)
+                      intent.putExtra("country",item.country)
+                      intent.putExtra("countryCode",item.alphaTwoCode)
+                      intent.putStringArrayListExtra("domains",item.domains)
+                      startActivity(intent)
+                  })
+              }
+
+
+      })
+  }
 
 
 }
